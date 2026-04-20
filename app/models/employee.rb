079e017122a25e8070ff355e5b0da8fc1f4357de
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-20 22:57:38 +0530

    Added validation for salary attribute

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ApplicationRecord
   validates :first_name, :last_name, :email, :job_title, :country, :salary, :phone_number, presence: true
+  validates :salary, numericality: { greater_than: 0 }
 end
