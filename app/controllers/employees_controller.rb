0279865d74bf07f9fefbaac6d059ce996d8b821c
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-21 00:54:39 +0530

    Added update endpoint for employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -20,6 +20,17 @@ class EmployeesController < ApplicationController
     render json: { error: 'Employee not found' }, status: :not_found
   end
 
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update(employee_params)
+      render json: @employee
+    else
+      render json: { errors: @employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Employee not found' }, status: :not_found
+  end
+
   private
   def employee_params
     params.require(:employee).permit(:first_name, :last_name, :job_title, :country, :salary, :email, :phone_number)
