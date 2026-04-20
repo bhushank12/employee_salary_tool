dba0c76c37baea59c129bdff27121ce85b2d68fe
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-20 22:52:41 +0530

    Added rspec for presence true attributes

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -6,4 +6,39 @@ RSpec.describe Employee, type: :model do
   it 'is valid with valid attributes' do
     expect(employee).to be_valid
   end
+
+  it 'is invalid without a first name' do
+    employee.first_name = nil
+    expect(employee).not_to be_valid
+  end
+
+  it 'is invalid without a last name' do
+    employee.last_name = nil
+    expect(employee).not_to be_valid
+  end
+
+  it 'is invalid without an email' do
+    employee.email = nil
+    expect(employee).not_to be_valid
+  end
+
+  it 'is invalid without a job title' do
+    employee.job_title = nil
+    expect(employee).not_to be_valid
+  end
+
+  it 'is invalid without a country' do
+    employee.country = nil
+    expect(employee).not_to be_valid
+  end
+
+  it 'is invalid without a salary' do
+    employee.salary = nil
+    expect(employee).not_to be_valid
+  end
+
+  it 'is invalid without a phone number' do
+    employee.phone_number = nil
+    expect(employee).not_to be_valid
+  end
 end
