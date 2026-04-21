418c3b6afc50605dc80e3348d5c7e8e7fc5073a9
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-21 17:23:38 +0530

    Added validation for phone number

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ApplicationRecord
   validates :first_name, :last_name, :email, :job_title, :country, :salary, :phone_number, presence: true
   validates :salary, numericality: { greater_than: 0 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :phone_number, format: { with: /\A\+?[0-9]{10,15}\z/, message: "must be a valid phone number" }
 
   def full_name
     "#{first_name} #{last_name}"
