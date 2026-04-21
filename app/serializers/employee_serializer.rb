13f8eaf3a3018e9f2c7314ec1036d3379c223439
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-21 17:13:14 +0530

    Added first and last name in serializer as it will be used in UI

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -2,6 +2,8 @@ class EmployeeSerializer
   def self.call(employee)
     {
       id: employee.id,
+      first_name: employee.first_name,
+      last_name: employee.last_name,
       full_name: employee.full_name,
       email: employee.email,
       job_title: employee.job_title,
