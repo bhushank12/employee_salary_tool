72d0389b35d70e1ce75f50248dc646021992ae0d
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-21 17:25:18 +0530

    Added errors check in test case for employee model

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -9,47 +9,65 @@ RSpec.describe Employee, type: :model do
 
   it 'is invalid without a first name' do
     employee.first_name = nil
+
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include("First name can't be blank")
   end
 
   it 'is invalid without a last name' do
     employee.last_name = nil
+
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include("Last name can't be blank")
   end
 
   it 'is invalid without an email' do
     employee.email = nil
+
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include("Email can't be blank")
   end
 
   it 'is invalid without a job title' do
     employee.job_title = nil
+
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include("Job title can't be blank")
   end
 
   it 'is invalid without a country' do
     employee.country = nil
+
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include("Country can't be blank")
   end
 
   it 'is invalid without a salary' do
     employee.salary = nil
+  
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include("Salary can't be blank")
   end
 
   it 'is invalid without a phone number' do
     employee.phone_number = nil
+
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include("Phone number can't be blank")
   end
 
   it 'is invalid if salary is less than or equal to 0' do
     employee.salary = 0
+
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include('Salary must be greater than 0')
   end
 
   it 'is invalid if salary is not a number' do
     employee.salary = 'not_a_number'
+  
     expect(employee).not_to be_valid
+    expect(employee.errors.full_messages).to include('Salary is not a number')
   end
 
   it 'return full name correctly' do
